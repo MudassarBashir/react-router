4ba4ef6203fbaa9c54b7f6bdd7b68dcf99b71479
Mudassar "Moe" Bashir <moeb59@gmail.com>
2020-02-11 21:58:57 -0500

    219. Set up Redux.

---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  /**
+   * TODO: Replace this dummy reducer introduced just so that Redux doesn't complain
+   * that it cannot find at least one Reducer.
+   * @returns {string}
+   */
+  replaceMe: () => 'asdf'
+});
